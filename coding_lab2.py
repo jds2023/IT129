3a8bf67a8f622aa1944e7cb4efd82b899e3d3926
Justin Singletary <justin_d_singletary@yahoo.com>
2023-02-04 19:11:08 -0500

    mpdified coding_lab2

--- coding_lab2.py
+++ coding_lab2.py
@@ -1,13 +1,7 @@
 group_info = []
 #Groups user information into a list
-group_info.append(input("What is the name of the reservations? ")
-group_info.append(input(int("How many people are coming? ")))
-group_info.append(input("Preferred location? (inside or outside) "))
+group_info.append(input("What is the name of the reservations? ").title())
+group_info.append(int(input("How many people are coming? ")))
+group_info.append(input("Preferred location? (inside or outside) ").lower())
 
-#User inputs for (), customers is integer
-# reservation_name = input("What is the name of the reservations? ")
-# customers = input("How many people are coming? ")
-# customers = int(customers)
-# location = input("Preferred location? (inside or outside) ")
-
-print(f"{group_info[0].capitalize()} booked a table for {group_info[1]} people. They want to eat {group_info[2].lower()}.")
+print(f"{group_info[0]}'s group booked a table for {group_info[1]} people. They want to eat {group_info[2]}.")
