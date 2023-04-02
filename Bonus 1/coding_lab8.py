1e91703b9d39f108e9d74414de5775d038175edb
Justin Singletary <justin_d_singletary@yahoo.com>
2023-04-02 17:26:20 -0400

    files edit

--- Bonus 1/coding_lab8.py
+++ Bonus 1/coding_lab8.py
@@ -1,14 +1,14 @@
 import os
-
 os.system("cls")
 
 while True:
-    user_input1 = input("Welcome to the Read/Writer APP\n\t1) Read\n\t2) Write\n\t3) Exit\nChoose between 1-3: ")
-    if (user_input1 == 1):
+    print("Welcome to the Read/Writer APP\n\t1) Read\n\t2) Write\n\t3) Exit\n")
+    user_input1 = int(input("Choose between 1-3: "))
+    if user_input1 == 1:
         user_input2 = input("You selected 'READ' Which file from? ")
         with open(user_input2, "r") as f:
             print(f.read())
-    elif (user_input1 == 2):
+    elif user_input1 == 2:
         user_input3 = input("You selected 'WRITE'. Which file to? ")
         user_input4 = input("What do you want to write? ")
         with open(user_input3, "w") as f:
