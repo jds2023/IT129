e7e65fb33c022d6013761689209134f7abe5c8d4
Justin Singletary <justin_d_singletary@yahoo.com>
2023-11-10 12:18:19 -0500

    bullets.py and more code

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -21,6 +21,10 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
             # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -29,6 +33,31 @@ class AlienInvasion:
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
+
+        
+    def _check_events(self):
+            """Respond to keypresses and mouse events."""
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                         self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False 
+                    elif event.key == pygame.K_LEFT:
+                         self.ship.moving_left = False 
+                        
+        
+    def _update_screen(self):
+            """Update images on screen, and flip to the new screen."""
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            pygame.display.flip()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
