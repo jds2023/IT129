e5fcc79baea79f5bb115a101cc6ed8aa51286da6
Justin Singletary <justin_d_singletary@yahoo.com>
2023-11-10 10:56:40 -0500

    more code

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -6,4 +6,5 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (60, 60, 60)
+        self.bg_color = (230, 230, 230)
+        
